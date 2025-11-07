8f7cca8fac2c51172cf47fe8d66f1e7187a85935
Thierry Schmidt <thierry@Mac.galaxus.box>
2025-11-07 07:12:00 +0100

    Update todoData.js

--- src/todoData.js
+++ src/todoData.js
@@ -4,14 +4,8 @@ export const todoData = {
     description: "",
     link: "https://www.google.com/",
     todoItems: [
-      "Erstellen Sie ein neues Word-Dokument",
-      "Fügen Sie eine Überschrift mit Formatvorlage 'Titel' hinzu",
-      "Schreiben Sie drei Absätze Text",
-      "Formatieren Sie den ersten Absatz mit Fettdruck",
-      "Fügen Sie eine nummerierte Liste mit 5 Punkten hinzu",
-      "Erstellen Sie eine Tabelle mit 3 Spalten und 4 Zeilen",
-      "Fügen Sie Seitenzahlen in der Fußzeile hinzu",
-      "Speichern Sie das Dokument als 'Übung_Woche5.docx'",
+      "Auftrag 1: Gruppe Schriftart",
+      "Auftrag 2: Überschrift mit Formatvorlage 'Titel'",
     ],
   },
 };
