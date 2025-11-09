3a81ba94ae316e4ad66a868f893874bcf7311169
tps <32765638+schmt5@users.noreply.github.com>
2025-11-09 12:26:31 +0100

    Update todoData.js

--- src/todoData.js
+++ src/todoData.js
@@ -2,7 +2,7 @@ export const todoData = {
   "pp-tabellen-folienmaster": {
     title: "Woche 5: Tabellen und Folienmaster",
     description: "",
-    link: "https://www.google.com/",
+    link: "https://erzbe.sharepoint.com/:f:/s/mpa25c-bemed/Eh_gDCsdE09CqEv-kUJ3qDYBaliJozpurq1Gw_On6Hip1Q?e=xBEdvq",
     todoItems: [
       "Auftrag 1: Tabellen",
       "Auftrag 2: Folienmaster - Produkt Pitch",
